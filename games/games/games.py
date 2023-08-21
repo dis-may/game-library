769c069c1ad0c0cd39d7e2ef63c0639b3c03b060
Elizabeth <lizzy4654@gmail.com>
2023-08-22 10:40:52 +1200

    flexboxes

--- games/games/games.py
+++ games/games/games.py
@@ -10,8 +10,8 @@ games_blueprint = Blueprint(
 
 
 @games_blueprint.route('/games', methods=['GET'])
-def get_genre_list_by_genre():
+def games_page():
     # Read query parameters.
     sort = request.args.get('sort')
-    games_list = services.get_game_list(repo.repo_instance)[:20]  # shows first 20 games for testing
+    games_list = services.get_game_list(repo.repo_instance)[:21]  # shows first 20 games for testing
     return render_template('games.html', games_list=games_list, genre_list=games.genres.services.get_list_of_genres(repo.repo_instance))
