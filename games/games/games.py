106f895ea0f2e2a7cb6d72818747cec28043e536
Elizabeth <lizzy4654@gmail.com>
2023-08-27 02:37:10 +1200

    added pagination

--- games/games/games.py
+++ games/games/games.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import request, render_template, url_for
 
+from math import ceil
+
 import games.games.services as services
 import games.adapters.repository as repo
 import games.genres.services
@@ -13,5 +15,24 @@ games_blueprint = Blueprint(
 def games_page():
     # Read query parameters.
     sort = request.args.get('sort')
-    games_list = services.get_game_list(repo.repo_instance)[:21]  # shows first 20 games for testing
-    return render_template('games.html', games_list=games_list, genre_list=games.genres.services.get_list_of_genres(repo.repo_instance))
+    page = request.args.get('page', 1, type=int)
+
+    # Calculate the range of games to display on the current page.
+    start_index = (page - 1) * 21
+    end_index = start_index + 21
+
+    all_games = services.get_game_list(repo.repo_instance)
+    # Get the list of games to display on the current page.
+    games_list = all_games[start_index:end_index]
+
+    # Total number of pages
+    total_pages = ceil(len(all_games) / 21)
+    print(total_pages)
+
+    return render_template('games.html',
+                           games_list=games_list,
+                           genre_list=games.genres.services.get_list_of_genres(repo.repo_instance),
+                           page=page,
+                           total_pages=total_pages,
+                           int=int
+                           )
