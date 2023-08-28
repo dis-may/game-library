5fe25f7a3a367145479f86c42e8985620f84cacc
Elizabeth Wu <104567240+Elizabeth0987@users.noreply.github.com>
2023-08-28 14:23:46 +1200

    css, game description draft

---
+++ games/gameDescription/gameDescription.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, render_template, url_for
+
+import games.games.services as services
+import games.adapters.repository as repo
+
+
+gameDescription_blueprint = Blueprint('gameDescription_bp', __name__)
+
+
+@gameDescription_blueprint.route('/gamesDescription', methods=['GET'])
+def game_desc_page():
+    # Read query parameters.
+    game_id = request.args.get('id')
+
+    # Get the game with the specified ID.
+    game = services.get_game(game_id, repo.repo_instance)
+
+    return render_template('gameDescription.html', game=game)
