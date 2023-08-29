caf97c970a4fe07927dde53c2d0860a762c0aef7
Elizabeth Wu <104567240+Elizabeth0987@users.noreply.github.com>
2023-08-29 16:07:03 +1200

    sorting draft

--- games/games/games.py
+++ games/games/games.py
@@ -17,8 +17,9 @@ def games_page():
     sort = request.args.get('sort')
     page = request.args.get('page', 1, type=int)
     genre = request.args.get('genre')
+    order = request.args.get('order')
 
-    # Calculate the range of games to display on the current page.
+    # Calculate the range of games to display on the current page
     start_index = (page - 1) * 21
     end_index = start_index + 21
 
@@ -32,6 +33,17 @@ def games_page():
 
     pagination_urls = [url_for('games_bp.games_page', page=i) for i in range(1, total_pages+1)]
 
+    # Sort the games if required.
+    if sort == 'title':
+        games_list.sort(key=lambda game: game.title)
+        if order == "desc":
+            games_list.sort(key=lambda game: game.title, reverse=True)
+
+    if sort == "price":
+        games_list.sort(key=lambda game: game.price)
+        if order == "desc":
+            games_list.sort(key=lambda game: game.price, reverse=True)
+
     return render_template('games.html',
                            games_list=games_list,
                            genre_url_dict=utilities.get_genre_url_dictionary(repo.repo_instance),
@@ -39,5 +51,7 @@ def games_page():
                            page=page,
                            total_pages=total_pages,
                            int=int,
-                           pagination_urls=pagination_urls
+                           pagination_urls=pagination_urls,
+                           sort=sort,
+                           order=order,
                            )
