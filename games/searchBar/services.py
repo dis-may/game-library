25015adf5b3a1f605244c8bd4f1b68fe56b6c87f
dis-may <maygan275@gmail.com>
2023-08-30 16:57:44 +1200

    you can sort genres, games and searches now

--- games/searchBar/services.py
+++ games/searchBar/services.py
@@ -1,32 +1,34 @@
 from games.adapters.repository import AbstractRepository
+from games.games.services import get_sorted_game_list
 from bisect import insort_left
 
 
-def get_games_by_search(repo: AbstractRepository, target_name: str):
-    games = repo.get_games_by_id()
+def get_games_by_title(repo: AbstractRepository, target_name: str, sort="id", order="asc"):
+    games = get_sorted_game_list(repo, sort, order)
     games_by_search = []
 
     for game in games:
         if target_name in game.title.lower():
-            insort_left(games_by_search, game)
+            games_by_search.append(game)
     return games_by_search
 
-def get_games_by_publisher(repo: AbstractRepository, target_name: str):
-    games = repo.get_games_by_id()
+
+def get_games_by_publisher(repo: AbstractRepository, target_name: str, sort="id", order="asc"):
+    games = get_sorted_game_list(repo, sort, order)
     games_by_publisher = []
 
     for game in games:
         if target_name in game.publisher.publisher_name.lower():
-            insort_left(games_by_publisher, game)
+            games_by_publisher.append(game)
     return games_by_publisher
 
-def get_games_by_description(repo: AbstractRepository, target_name: str):
-    games = repo.get_games_by_id()
+
+def get_games_by_description(repo: AbstractRepository, target_name: str, sort="id", order="asc"):
+    games = get_sorted_game_list(repo, sort, order)
     games_by_description = []
 
     for game in games:
         if game.description is not None:
             if target_name in game.description.lower():
-                insort_left(games_by_description, game)
+                games_by_description.append(game)
     return games_by_description
-
