741e2cfd1002b07dd1c1b2ad13ea3696e722dbe6
akim525 <akim525@aucklanduni.ac.nz>
2023-09-22 17:40:32 +1200

    make some changes in wishlist but not finihsed

--- games/wishlist/wishlist.py
+++ games/wishlist/wishlist.py
@@ -4,11 +4,19 @@ import games.utilities.utilities as utilities
 
 wishlist_blueprint = Blueprint('wishlist_bp', __name__)
 
-
-@wishlist_blueprint.route('/wishlist', methods=['GET'])
+wishlist = []
+def list_append(game):
+    global wishlist
+    wishlist.append(game)
+@wishlist_blueprint.route('/wishlist', methods=['GET', 'POST'])
 def games_wishlist():
+    if request.method == "POST":
+        game_added = request.form["game"]
+        list_append(game_added)
+
     return render_template('wishlist.html',
-                           genre_url_dict=utilities.get_genre_url_dictionary(repo.repo_instance)
+                           genre_url_dict=utilities.get_genre_url_dictionary(repo.repo_instance),
+                           wishlist = wishlist
                            )
 
 
