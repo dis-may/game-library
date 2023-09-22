ca534677f573ccd5b6d5d3cc413c0333c6dcc386
Elizabeth Wu <104567240+Elizabeth0987@users.noreply.github.com>
2023-09-22 14:06:50 +1200

    made certain functions only available on login

--- games/wishlist/wishlist.py
+++ games/wishlist/wishlist.py
@@ -3,10 +3,13 @@ from flask import Blueprint, request, render_template, url_for
 import games.adapters.repository as repo
 import games.utilities.utilities as utilities
 
+from games.authentication.authentication import login_required
+
 wishlist_blueprint = Blueprint('wishlist_bp', __name__)
 
 
 @wishlist_blueprint.route('/wishlist', methods=['GET'])
+@login_required
 def games_wishlist():
     return render_template('wishlist.html',
                            genre_url_dict=utilities.get_genre_url_dictionary(repo.repo_instance)
