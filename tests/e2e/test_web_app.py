d20ffa7c03096eb19a0d7b829fced0c98546f8da
dis-may <maygan275@gmail.com>
2023-09-24 20:30:15 +1300

    e2e for testing registering

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -3,20 +3,34 @@ import pytest
 from flask import session
 
 
-# def test_register(client):
-#     # Test that we can GET the register page successfully
-#     response_code = client.get('/').status_code
-#     assert response_code == 200
-#
-#     # Test that we can register a user (POST), supplying a valid user name and password.
-#     response = client.post(
-#         '/authentication/register',
-#         data={'user_name': 'gmichael', 'password': 'CarelessWhisper1984'}
-#     )
-#     assert response.headers['Location'] == 'http://localhost/authentication/login'
-
-def test_index(client):
+def test_home(client):
     # Check that we can retrieve the home page.
     response = client.get('/')
     assert response.status_code == 200
+    assert b'My Game Library' in response.data
 
+
+def test_games_page(client):
+    response = client.get('/games')
+    assert response.status_code == 200
+    assert b'All Games' in response.data
+
+
+def test_register(client):
+    # check that we can GET the register page in order to enter details
+    response = client.get('/authentication/register')
+    assert response.status_code == 200
+
+    # check that we can POST user details to successfully make an account
+    response = client.post(
+        '/authentication/register',
+        data={'name': 'Alice', 'user_name': 'al', 'password': 'Password123'},
+    )
+    response = client.post(
+        '/authentication/login',
+        data={'user_name': 'alice123', 'password': 'Wonderland123'},
+        follow_redirects=True
+    )
+    # The only page which has <p>Login</p> is the login.html page, so this confirms that is redirects
+    # successfully to the login page after registering
+    assert b'<p>Login</p>' in response.data
